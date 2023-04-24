4db57e7411b9c5fa8f3ab64a2b14e4567ebbe695
Jean <acalto2@gmail.com>
2023-04-24 21:17:53 +0200

    4. Obscure Sensitive Data.mp4

---
+++ lib/core/providers/flavor_provider.dart
@@ -0,0 +1,7 @@
+
+import 'package:ecomm_app/core/flavor/flavor.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final flavorProvider = StateProvider<Flavor>((ref) {
+  return Flavor.dev;
+});
